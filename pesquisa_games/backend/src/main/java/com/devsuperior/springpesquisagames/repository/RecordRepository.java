d437325a3de83a4e5bbccb2f5d370024ab5ab913
fern <felipe.ghidini@gft.com>
2020-12-09 22:02:31 -0300

    Iniciado a migraçao para nuvem

--- pesquisa_games/backend/src/main/java/com/devsuperior/springpesquisagames/repository/RecordRepository.java
+++ pesquisa_games/backend/src/main/java/com/devsuperior/springpesquisagames/repository/RecordRepository.java
@@ -3,7 +3,7 @@ package com.devsuperior.springpesquisagames.repository;
 import java.time.Instant;
 
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -14,7 +14,15 @@ import com.devsuperior.springpesquisagames.model.Record;
 public interface RecordRepository extends JpaRepository<Record, Long>{
 
 	
-	@Query("SELECT obj FROM Record obj")
-	Page<Record> findByMoments(Instant min, Instant max, PageRequest pageRequest);
+	@Query("SELECT obj FROM Record obj WHERE "
+			+ "(:min IS NULL OR obj.moment >= :min) AND "
+			+ "(:max IS NULL OR obj.moment <= :max)")
+	Page<Record> findByMoments(Instant min, Instant max, Pageable pageable);
 
 }
+
+
+////Para o postgree utiliza-se assim, ver no MySQL
+//("SELECT obj FROM Record obj WHERE "
+//		+ "(coalesce(:min, null) IS NULL OR obj.moment >= :min) AND "
+//		+ "(coalesce(:max, null) IS NULL OR obj.moment <= :max)")
